ef2a1abc6b83fa98193a3b4a461dd3acbbed6756
tyronedarby <58233457+tyronedarby@users.noreply.github.com>
2026-09-16 14:24:43 -0400

    Update formatting

--- Assignment5/src/csci3236/assignment5/Assignment5.java
+++ Assignment5/src/csci3236/assignment5/Assignment5.java
@@ -8,7 +8,9 @@ import java.util.regex.Pattern;
 public class Assignment5 {
 
     public static void printRegEx(String regex, int n) {
+        
     }
+
     public static void main(String[] args) {
         printRegEx("01*|(10)*", 5);
     }
