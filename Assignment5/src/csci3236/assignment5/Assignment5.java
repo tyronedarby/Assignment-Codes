14944b8bf372874f2b8999be05d0b58e1c2276a3
tyronedarby <58233457+tyronedarby@users.noreply.github.com>
2026-09-16 14:19:07 -0400

    Initial commit

---
+++ Assignment5/src/csci3236/assignment5/Assignment5.java
@@ -0,0 +1,15 @@
+package csci3236.assignment5;
+
+import java.util.regex.Pattern;
+/**
+ *
+ * @author Tyrone Darby
+ */
+public class Assignment5 {
+
+    public static void printRegEx(String regex, int n) {
+    }
+    public static void main(String[] args) {
+        printRegEx("01*|(10)*", 5);
+    }
+}
