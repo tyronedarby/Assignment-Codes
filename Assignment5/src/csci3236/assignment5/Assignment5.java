3706fd25b6c7a98a1bcb84caa0e8f4af64c9f572
tyronedarby <58233457+tyronedarby@users.noreply.github.com>
2026-09-18 09:14:39 -0400

    Implement the printRegEx

--- Assignment5/src/csci3236/assignment5/Assignment5.java
+++ Assignment5/src/csci3236/assignment5/Assignment5.java
@@ -1,5 +1,7 @@
 package csci3236.assignment5;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.regex.Pattern;
 /**
  *
@@ -8,7 +10,27 @@ import java.util.regex.Pattern;
 public class Assignment5 {
 
     public static void printRegEx(String regex, int n) {
-        
+        Pattern pattern = Pattern.compile(regex);
+        Queue<String> queue = new ArrayDeque<>();
+
+        queue.add("");
+
+        while (!queue.isEmpty()){
+            String current = queue.poll();
+
+            if (pattern.matcher(current).matches()){
+                if (current.isEmpty()){
+                    System.out.println("\u03B5");
+                } else {
+                    System.out.println(current);
+                }
+            }
+
+            if (current.length() < n){
+                queue.add(current + "0");
+                queue.add(current + "1");
+            }
+        }
     }
 
     public static void main(String[] args) {
